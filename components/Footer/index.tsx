589ef0257147ca7c496cb476a2de2ecc9d3d981c
anastasia rogachevskaya <anastasia.rogachevskaya@relexsolutions.com>
2023-10-11 19:31:43 +0300

    Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,53 +1,77 @@
 import React from 'react';
 import styles from './Footer.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer: React.FC = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.text20}>We serve in Finnish and English</div>
-      <div className={styles.block}>
-        <a
-          href="https://facebook.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="/path-to-facebook-icon.svg"
-            alt="Facebook"
-            className={styles.icon}
-          />
-        </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <img
-            src="/path-to-twitter-icon.svg"
-            alt="Twitter"
-            className={styles.icon}
-          />
-        </a>
-        <a
-          href="https://instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="/path-to-instagram-icon.svg"
-            alt="Instagram"
-            className={styles.icon}
-          />
-        </a>
+    <>
+      <div className={styles.backgroundImage}>
+        <Image
+          src="/img/card.jpg"
+          alt="Card"
+          width="1024"
+          height="555"
+          quality={100}
+          className="card"
+        />
       </div>
-      <div className={styles.text14}>
-        <Link href={'mailto:info@finmairs.fi'}>info@finmairs.fi</Link>, 044 517
-        4878, Sahaajankatu 24, 00880 Helsinki{' '}
-      </div>
-      <div className={styles.text14}>
-        <div>© Finmairs — All rights reserved</div>
-        <Link href={'XXX'} target="_blank" className={styles.link}>
-          Tietosuojaseloste
-        </Link>
-      </div>
-    </footer>
+
+      <footer className={styles.footer}>
+        <div className={styles.text20}>We serve in Finnish and English</div>
+        <div className={styles.block}>
+          <Link
+            href="https://whatsapp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/img/icon_whatsapp.svg"
+              alt="WhatsApp"
+              width={20}
+              height={20}
+              className={styles.icon}
+            />
+          </Link>
+          <Link
+            href="https://Instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/img/icon_instagram.svg"
+              alt="Instagram"
+              width={20}
+              height={20}
+              className={styles.icon}
+            />
+          </Link>
+          <Link
+            href="https://messenger.com" // TODO: change this
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="img/icon_messenger.svg"
+              alt="Messenger"
+              width={20}
+              height={20}
+              className={styles.icon}
+            />
+          </Link>
+        </div>
+        <div className={styles.text14}>
+          <Link href={'mailto:info@finmairs.fi'}>info@finmairs.fi</Link>, 044
+          517 4878, Sahaajankatu 24, 00880 Helsinki{' '}
+        </div>
+        <div className={styles.text14}>
+          <div>© Finmairs — All rights reserved</div>
+          <Link href={'XXX'} target="_blank" className={styles.link}>
+            Tietosuojaseloste
+          </Link>
+        </div>
+      </footer>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import Image from 'next/image';
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Welcome to My Static Site!</h1>
-      <p>This is a static single page app built with React and Next.js using TypeScript.</p>
+      <p>
+        This is a static single page app built with React and Next.js using
+        TypeScript.
+      </p>
+
       <Footer />
     </div>
   );
-}
+};
 
 export default Home;
