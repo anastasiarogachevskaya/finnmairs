b9389fa124d29fe984b5a5c91c88efc8a3928d32
anastasia rogachevskaya <anastasia.rogachevskaya@relexsolutions.com>
2023-10-11 17:55:01 +0300

    typescript + gitignore

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Static Site!</h1>
+      <p>This is a static single page app built with React and Next.js using TypeScript.</p>
+    </div>
+  );
+}
+
+export default Home;
